46a0a094020bf101e34dae45fc61048c4689fe24
강진우 <wlsdn1252@naver.com>
2023-03-29 00:11:34 +0900

    숫자를 입력 받기 위한 변수 선언

--- app/src/main/java/com/example/calcurater/MainActivity.kt
+++ app/src/main/java/com/example/calcurater/MainActivity.kt
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    // 계산기에서 바뀌는 값을 받기위해 변수 선언.
+    private val firstNumberText = StringBuilder("")
+    private val secondNumberText = StringBuilder("")
+    private val operatorText = StringBuilder("")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
